841089a111e9ae6bf29331970e618004ba1a0df8
Linus Groner <linus.groner@cscs.ch>
2022-02-09 16:40:15 +0100

    get dtype more nicely in __sdfg__, enable AOT compilation case

--- tests/test_integration/test_dace_parsing.py
+++ tests/test_integration/test_dace_parsing.py
@@ -129,6 +129,48 @@ def test_optional_arg_provide():
     assert np.sum(outp, axis=(0, 1, 2)) == 90 * 7.0
 
 
+def test_optional_provide_aot():
+    @gtscript.stencil(backend="gtc:dace")
+    def stencil(
+        inp: gtscript.Field[np.float64],
+        unused_field: gtscript.Field[np.float64],
+        outp: gtscript.Field[np.float64],
+        unused_par: float,
+    ):
+        with computation(PARALLEL), interval(...):
+            outp = inp  # noqa F841: local variable 'outp' is assigned to but never used
+
+    frozen_stencil = stencil.freeze(
+        domain=(3, 3, 10), origin={"inp": (2, 2, 0), "outp": (2, 2, 0), "unused_field": (0, 0, 0)}
+    )
+
+    inp = np.full(fill_value=7.0, dtype=np.float64, shape=(10, 10, 10))
+    outp = np.zeros(dtype=np.float64, shape=(10, 10, 10))
+    unused_field = np.zeros(dtype=np.float64, shape=(10, 10, 10))
+
+    @dace.program
+    def call_frozen_stencil(
+        inp: dace.float64[inp.shape],  # type: ignore
+        outp: dace.float64[outp.shape],  # type: ignore
+        unused_field: dace.float64[unused_field.shape],  # type: ignore
+        unused_par: dace.float64,
+    ):
+        frozen_stencil(inp=inp, unused_field=unused_field, outp=outp, unused_par=unused_par)
+
+    csdfg = call_frozen_stencil.compile()
+    csdfg(inp=inp, outp=outp, unused_field=unused_field, unused_par=7.0)
+    assert np.allclose(inp, 7.0)
+    assert np.allclose(
+        outp[
+            2:5,
+            2:5,
+            :,
+        ],
+        7.0,
+    )
+    assert np.sum(outp, axis=(0, 1, 2)) == 90 * 7.0
+
+
 @pytest.mark.xfail(
     reason="DaCe catches all exceptions in __sdfg__ interface, hence the Exception Type and string don't"
     " match."
