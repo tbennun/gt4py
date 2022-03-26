7ac3bbdfdfab45fa3cccb6c29d66176196e0908f
Linus Groner <linus.groner@cscs.ch>
2022-03-26 13:34:28 +0100

    support regions in if statements and if statements in regions

--- src/gtc/passes/gtir_remove_regions.py
+++ src/gtc/passes/gtir_remove_regions.py
@@ -38,14 +38,17 @@ class RemoveUnexecutedRegions(eve.NodeTranslator):
         vertical_loops = [
             vloop for vloop in reversed(rev_vertical_loops) if isinstance(vloop, gtir.VerticalLoop)
         ]
+        accessed_fields = (
+            iter_tree(vertical_loops).if_isinstance(gtir.FieldAccess).getattr("name").to_set()
+        )
+        accessed_scalars = (
+            iter_tree(vertical_loops).if_isinstance(gtir.ScalarAccess).getattr("name").to_set()
+        )
         params = [
             decl
             for decl in node.params
-            if decl.name
-            in iter_tree(vertical_loops)
-            .if_isinstance(gtir.FieldAccess, gtir.ScalarAccess)
-            .getattr("name")
-            .to_set()
+            if (isinstance(decl, gtir.FieldDecl) and decl.name in accessed_fields)
+            or (isinstance(decl, gtir.ScalarDecl) and decl.name in accessed_scalars)
         ]
         return gtir.Stencil(
             name=node.name,
@@ -63,7 +66,6 @@ class RemoveUnexecutedRegions(eve.NodeTranslator):
                 if decl.name
                 in iter_tree(stmts).if_isinstance(gtir.FieldAccess).getattr("name").to_set()
             ]
-
             return gtir.VerticalLoop(
                 interval=node.interval,
                 loop_order=node.loop_order,
@@ -76,13 +78,55 @@ class RemoveUnexecutedRegions(eve.NodeTranslator):
     def visit_HorizontalRegion(self, node: gtir.HorizontalRegion, *, ctx):
         res_stmts = []
         for stmt in node.block.body:
+            new_stmt = self.visit(stmt, ctx=ctx)
             ext_diff = utils.compute_extent_difference(ctx.stmt_extents[id(stmt)], node.mask)
             if ext_diff is not None:
-                res_stmts.append(stmt)
+                res_stmts.append(new_stmt)
         if not res_stmts:
             return eve.NOTHING
         else:
+            res_extent = None
+            for stmt in node.block.body:
+                if res_extent is None:
+                    res_extent = ctx.stmt_extents[id(stmt)]
+                else:
+                    res_extent |= ctx.stmt_extents[id(stmt)]
+            ctx.stmt_extents[id(node)] = res_extent
             return gtir.HorizontalRegion(mask=node.mask, block=gtir.BlockStmt(body=res_stmts))
+
+    def _visit_IfStmt(self, node, *, ctx):
+        res = None
+        stmts = node.true_branch.body
+        new_true_branch = self.generic_visit(node.true_branch.body, ctx=ctx)
+        if node.false_branch is not None:
+            stmts += node.false_branch.body
+            new_false_branch = self.generic_visit(node.false_branch.body, ctx=ctx)
+        else:
+            new_false_branch = None
+        for stmt in stmts:
+            self.visit(stmt, ctx=ctx)
+            extent = ctx.stmt_extents[id(stmt)]
+            if res is None:
+                res = extent
+            else:
+                res |= extent
+        ctx.stmt_extents[id(node)] = res
+        return (
+            gtir.BlockStmt(body=new_true_branch),
+            gtir.BlockStmt(body=new_false_branch) if new_false_branch is not None else None,
+        )
+
+    def visit_FieldIfStmt(self, node: gtir.FieldIfStmt, **kwargs):
+        new_true_branch, new_false_branch = self._visit_IfStmt(node, **kwargs)
+        return gtir.FieldIfStmt(
+            cond=node.cond, true_branch=new_true_branch, false_branch=new_false_branch
+        )
+
+    def visit_ScalarIfStmt(self, node: gtir.ScalarIfStmt, **kwargs):
+        new_true_branch, new_false_branch = self._visit_IfStmt(node, **kwargs)
+        return gtir.ScalarIfStmt(
+            cond=node.cond, true_branch=new_true_branch, false_branch=new_false_branch
+        )
 
 
 def remove_regions(node):
